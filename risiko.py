0a555d38f5d0de36cb52cf6962658595a43b6d9e
dome <federicodomeniconi@gmail.com>
2019-09-22 00:58:41 +0200

    Little fix.

--- risiko.py
+++ risiko.py
@@ -218,7 +218,7 @@ def plot_heatmap(matrix, label):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='description')
+    parser = argparse.ArgumentParser(description='A program that, given the armies size of the attacker and the defender, calculates the probability of winning the battle and the expected losses of the attacker using Markov chain.')
     parser.add_argument('-b', '--boost', default=False, action='store_true', help='Boost CPU computation with PyTorch')
     parser.add_argument('-p', '--plot', default=False, action='store_true', help='Plot the heatmaps')
 
